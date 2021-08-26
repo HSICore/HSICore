72f78c306e1978aad4f88afc78c769a2067f57a6
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2021-08-27 08:57:39 +1000

    added some files

--- Electron/SrcJS/preload.js
+++ Electron/SrcJS/preload.js
@@ -1,5 +1,9 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+var electron = require("electron")
+
+var bcrypt = require("bcryptjs")
+
+var fs = requrie("fs")
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
@@ -10,4 +14,36 @@ window.addEventListener('DOMContentLoaded', () => {
       replaceText(`${type}-version`, process.versions[type])
     }
   })
-  
+
+electron.contextBridge.exposeInMainWorld("plapi",
+{
+    fs = {
+        writeFile = function(text, name) {
+            fs.writeFileSync(`./Data/${name}`)
+        },
+        fileExists = function(name) {
+            return fs.existsSync(`./Data/${name}`)
+        },
+        readFile = function(name) {
+            if (!fs.existsSync(`./Data/${name}`)) {
+                throw new ReferenceError("Cannot load a nonexistent file.")
+            } else {
+                return fs.readFileSync(`./Data/${name}`)
+            }
+        }
+    },
+    bcrypt = {
+        compareEncryptedString = function(string, hash) {
+
+            if (string == null || string == undefined) {
+                throw new SyntaxError("Cannot compare an null or undefined string to a hash.")
+            }
+
+            if (hash == null || hash == undefined) {
+                throw new SyntaxError("Cannot compare an null or undefined hash to a string.")
+            }
+
+            return bcrypt.compareSync(string, hash)
+        }
+    }
+})
